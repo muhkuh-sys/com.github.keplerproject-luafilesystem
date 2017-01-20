ae5dc6669aeba4e14116402695713d58864ee6a0
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-01-20 19:48:50 +0100

    Use jonchki to get the build requirements.

---
+++ .build_common.sh
@@ -0,0 +1,30 @@
+# Do not call this script directly. It is included from the .build??_*.sh files.
+
+echo "PRJ_DIR        = ${PRJ_DIR}"
+echo "BUILD_DIR      = ${BUILD_DIR}"
+echo "CMAKE_COMPILER = ${CMAKE_COMPILER}"
+echo "JONCHKI        = ${JONCHKI}"
+echo "JONCHKI_SYSTEM = ${JONCHKI_SYSTEM}"
+
+# Create all folders.
+rm -rf ${BUILD_DIR}
+mkdir -p ${BUILD_DIR}
+mkdir ${BUILD_DIR}/lua5.1
+mkdir ${BUILD_DIR}/lua5.1/build_requirements
+mkdir ${BUILD_DIR}/lua5.2
+mkdir ${BUILD_DIR}/lua5.2/build_requirements
+mkdir ${BUILD_DIR}/lua5.3
+mkdir ${BUILD_DIR}/lua5.3/build_requirements
+
+# Get the build requirements for the LUA5.1 version.
+pushd ${BUILD_DIR}/lua5.1/build_requirements
+cmake -DBUILDCFG_ONLY_JONCHKI_CFG="ON" -DCMAKE_INSTALL_PREFIX="" ${CMAKE_COMPILER} ${PRJ_DIR}
+make
+lua5.1 ${JONCHKI} --verbose debug --syscfg ${PRJ_DIR}/jonchki/jonchkisys.cfg --prjcfg ${PRJ_DIR}/jonchki/jonchkicfg.xml ${JONCHKI_SYSTEM} --build-dependencies luafilesystem/lua5.1-luafilesystem-*.xml
+popd
+
+# Build the LUA5.1 version.
+pushd ${BUILD_DIR}/lua5.1
+cmake -DBUILDCFG_LUA_USE_SYSTEM="OFF" -DBUILDCFG_LUA_VERSION="5.1" -DCMAKE_INSTALL_PREFIX="" ${CMAKE_COMPILER} ${PRJ_DIR}
+make
+popd
